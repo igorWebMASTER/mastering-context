a9bec3c40e346a84f587a941ebea31348d366d75
igor.sobral <igor.sobral@pagtel.com.br>
2022-02-14 23:59:14 -0300

    feat: add counter with use state

---
+++ pages/counter/counter-use-state.js
@@ -0,0 +1,35 @@
+
+import { useState } from 'react'
+
+const Container = ({ children }) => (
+    <div>
+       {children}
+    </div>
+)
+
+
+const AddOneButton  = ({ setCounter }) => {
+    return (
+         <div>
+            <button onClick={() => setCounter((v) => v + 1)}>Add One</button>
+         </div>
+    )
+}
+
+
+const Counter = ({counter}) => (
+    <div>Counter: {counter}</div>
+)
+
+export default function CounterUseState() {
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+            <Container>
+                <AddOneButton setCounter={setCounter} />
+            </Container>
+            <Counter counter={counter} />
+        </div>
+    )
+}
