835855535ec509bf0110be4dd760abd777c4b7de
igor.sobral <igor.sobral@pagtel.com.br>
2022-02-15 00:02:53 -0300

    chore: add store of zustand

---
+++ pages/ecommerce/store-zustand.js
@@ -0,0 +1,18 @@
+import create from 'zustand';
+
+const useStore = create(set => ({
+    user: "",
+    cartCount: 0,
+    login: () => set(state => ({ user: "Jack" })),
+    logout: () => set(state => ({ user: "" })),
+    addToCart: () => set(state => ({ cartCount: state.cartCount + 1 })),
+}))
+
+export default useStore;
+
+
+export const useLogin = () => useStore((state) => state.login);
+export const useLogout = () => useStore((state) => state.logout);
+export const useAddToCart = () => useStore((state) => state.addToCart);
+export const useCartCount = () => useStore((state) => state.cartCount);
+export const useUser = () => useStore((state) => state.user);
